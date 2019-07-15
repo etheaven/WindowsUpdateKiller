11727604d21a4599566985c7b739e1bc4adca3c7
ssoka <ssoka@MHCH-PC>
2019-07-15 09:24:02 +0200

    changed

--- WinUpdateKiller/Manager.cs
+++ WinUpdateKiller/Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.ServiceProcess;
@@ -14,23 +15,55 @@ namespace WinUpdateKiller
     {
         public Manager()
         {
-           Task.Run(delegate
+           Task.Factory.StartNew(delegate
            {
+               List<string> servicenames = new List<string>(){"wuauserv", "UsoSvc", "WaaSMedicSvc", "WSearch" };
+               if(File.Exists("services.cfg"))
+               using (var file = new System.IO.StreamReader("services.cfg"))
+               {
+                   var split = file.ReadLine().Split(';');
+                   servicenames = split.ToList();
+               }
+               else
+               {
+                   using (var file = new StreamWriter("services.cfg"))
+                   {
+                       file.WriteLine($"wuauserv;UsoSvc;WaaSMedicSvc;WSearch");
+                   }
+               }
                while (true)
                {
-                   var serviceName = "wuauserv";
-                   ServiceController service = new ServiceController("Windows Update");
-                  
-                   if ((service.Status.Equals(ServiceControllerStatus.Stopped)) ||
-
-                       (service.Status.Equals(ServiceControllerStatus.StopPending)))
+                   //var serviceName = "wuauserv";//"Windows Update"
+                   foreach (string serviceName in servicenames)
                    {
-                       using (var m = new ManagementObject(string.Format("Win32_Service.Name=\"{0}\"", serviceName)))
+                       ServiceController service = new ServiceController(serviceName);
+                  
+                       if ((service.Status.Equals(ServiceControllerStatus.Stopped)) ||
+
+                           (service.Status.Equals(ServiceControllerStatus.StopPending)))
                        {
-                           m.InvokeMethod("ChangeStartMode", new object[] { "Disabled" });
+                           using (var m = new ManagementObject(string.Format("Win32_Service.Name=\"{0}\"", serviceName)))
+                           {
+                               try
+                               {
+                                   m.InvokeMethod("ChangeStartMode", new object[] { "Disabled" });
+                               }
+                               catch (Exception e)
+                               {
+                                   
+                               }
+                           }
                        }
+                       else
+                           try
+                           {
+                               service.Stop();
+                           }
+                           catch (Exception e)
+                           {
+
+                           }
                    }
-                   else service.Stop();
                    Thread.Sleep(1000);
                }
            });
